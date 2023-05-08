76c53d501522959db66817f2b493bee37e831cbd
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-05-09 04:11:53 +0900

    Commit02_0509

--- app/src/main/java/com/app/perfumerecommendation_project02/LoginActivity.java
+++ app/src/main/java/com/app/perfumerecommendation_project02/LoginActivity.java
@@ -58,7 +58,7 @@ public class LoginActivity extends AppCompatActivity {
                                 String userPass=jsonObject.getString("userPassword");
 
                                 Toast.makeText(getApplicationContext(),"로그인에 성공하셨습니다.",Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, IdPassViewActivity.class);
                                 intent.putExtra("userID",userID);
                                 intent.putExtra("userPass",userPass);
                                 startActivity(intent);
